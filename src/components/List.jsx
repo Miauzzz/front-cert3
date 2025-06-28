c2d62e6ca15f5059696aa56d705b371eeeb2b226
Manuel Alejandro C. <120736944+Miauzzz@users.noreply.github.com>
2025-06-28 19:36:11 -0400

    Arreglos en List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import Item from './Item';
 
 function List({ items, deleteItem, editItem }) {
-    return (
-        <ul>
-            {items.map((item) => (
-                <Item
-                    key={item.id}
-                    item={item}
-                    deleteItem={deleteItem}
-                    editItem={editItem}
-                />
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {items.length === 0 ? (
+        <li style={{
+          background: 'none',
+          boxShadow: 'none',
+          border: 'none',
+          color: '#888',
+          textAlign: 'center'
+        }}>
+          No hay evaluaciones guardadas.
+        </li>
+      ) : (
+        items.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            deleteItem={deleteItem}
+            editItem={editItem}
+          />
+        ))
+      )}
+    </ul>
+  );
 }
 
 export default List;
