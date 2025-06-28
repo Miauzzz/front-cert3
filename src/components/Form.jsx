992f8166317fa21da17642d69c27d223542c34e2
Manuel Alejandro C. <120736944+Miauzzz@users.noreply.github.com>
2025-06-28 19:35:03 -0400

    Arreglos en Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,62 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useRef } from 'react';
 function Form({ addOrUpdateItem, itemToEdit }) {
-    const [inputValue, setInputValue] = useState("");
+const [nombre, setNombre] = useState('');
+const [asignatura, setAsignatura] = useState('');
+const [promedio, setPromedio] = useState('');
+const nombreRef = useRef(null);
 
-    useEffect(() => {
-        if (itemToEdit) {
-            setInputValue(itemToEdit.value);
-        } else {
-            setInputValue("");
-        }
-    }, [itemToEdit]);
+useEffect(() => {
+    if (itemToEdit) {
+      setNombre(itemToEdit.value?.nombre || '');
+      setAsignatura(itemToEdit.value?.asignatura || '');
+      setPromedio(itemToEdit.value?.promedio || '');
+    } else {
+      setNombre('');
+      setAsignatura('');
+      setPromedio('');
+    }
+    nombreRef.current && nombreRef.current.focus();
+}, [itemToEdit]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (inputValue.trim()) {
-        addOrUpdateItem(inputValue);
-        setInputValue("");
-        }
-    };
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (nombre.trim() && asignatura.trim() && promedio.trim()) { 
+      addOrUpdateItem({ nombre, asignatura, promedio });
+      setNombre('');
+      setAsignatura('');
+      setPromedio('');
+    }
+};
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-            />
-            <button type="submit">{itemToEdit ? "Actualizar" : "Agregar"}</button>
-        </form>
-    );
+return (
+    <form className="form-eval" onSubmit={handleSubmit}>
+      <input
+        ref={nombreRef}
+        type="text"
+        placeholder="Ej: Andres Navarro"
+        value={nombre}
+        onChange={e => setNombre(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ej: Bases de datos"
+        value={asignatura}
+        onChange={e => setAsignatura(e.target.value)}
+      />
+      <input
+        type="number"
+        step="0.1"
+        min="0"
+        max="7"
+        placeholder="Ej: 5.0"
+        value={promedio}
+        onChange={e => setPromedio(e.target.value)}
+      />
+      <button type="submit">
+        {itemToEdit ? 'Actualizar Evaluación' : 'Agregar Evaluación'}
+      </button>
+    </form>
+);
 }
 
 export default Form;
